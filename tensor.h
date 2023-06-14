73ec083f6e140c52d4b71641e239d4770350a47c
Octavian Rusu <octavian.concept@gmail.com>
2023-06-14 01:37:06 -0600

    private function to check if view is valid for tensor size, very primitive shape getter and reshape function

--- tensor.h
+++ tensor.h
@@ -1,11 +1,15 @@
 
 #ifndef TENSOR
 #define TENSOR
-#endif
+
+#define TENSOR_MAX_DIM 2 << 15
+#define TENSOR_MAX_STORAGE_SIZE 2 << 63 
 
 #include <array>
 
 using std::size_t;
+
+namespace tensor {
 
 struct View {
 	uint32_t* view;
@@ -23,24 +27,36 @@ class Tensor {
 	public:
 		Tensor(T* arr, std::initialiser_list<uint32_t> shape) 
 			: storage(*arr)
-		{
-			int acc = 0;
-			for(const auto& s: *shape) { acc *= *s; }
-			if(N % acc == 0) this->shape(*shape)
-			else {
-				// RAISE ERROR
-			}
-		};
+		{ };
 
-		//std::array<std::size_t, this->shape.view.size()> shape() {
-		//	return this->shape.view;
-		//}
+		uint32_t* shape() {
+			return this->shape.view;
+		}
+
+		bool reshape(std::initializer_list<uint32_t> nview) {
+			if(this->is_valid_view(nview)) {
+				this->shape.view = nview;
+				return 1;
+			}
+			return 0;
+		}
 
 	private:
+		uint64_t storage_size;
 		std::array<T, N> storage;
 		View shape;
+
+		bool is_valid_view(std::initializer_list<uint32_t> shape) {
+			uint16_t p = 1;
+			for(const auto& i : *shape) { p *= *i; }
+			if(this->storage_size % p == 0) return 1;	
+			else return 0;
+		}
 };
 
 
 // uint32_t arr[] = { . . . };
 // auto a = Tensor<uint32_t, arr.size()>( arr, {2, 2, 1} );
+
+} // namespace
+#endif
