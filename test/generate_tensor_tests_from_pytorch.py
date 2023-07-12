f48902d952056af754194c0b03826bac82ae9fe9
Octavian Rusu <octavian.concept@gmail.com>
2023-07-11 21:42:36 -0600

    move per element test to catch2

--- test/generate_tensor_tests_from_pytorch.py
+++ test/generate_tensor_tests_from_pytorch.py
@@ -7,13 +7,14 @@ DIM = 4
 def check_each_value_against_pytorch():
     f = open("test_tensor_indexing_with_pytorch.cc", "w")
     rep = """
+#define CATCH_CONFIG_MAIN 
 #include <iostream>
-#include <cassert>
 #include "tensor.h"
+#include "catch.hpp"
 
 using namespace tensor;
 
-bool test_indexing_from_pytorch() {
+TEST_CASE("test_indexing_from_pytorch") {
 """
     rep += f"\tstd::unique_ptr<float[]> data = std::make_unique<float[]>({N});\n"
     rep += f"\tfor(size_t i=0; i < {N}; i++) {{ data[i]=i; }}\n"
@@ -26,15 +27,8 @@ bool test_indexing_from_pytorch() {
         for j in range(0, len(t[0])):
             for k in range(0, len(t[0, 0])):
                 for l in range(0, len(t[0, 0, 0])):
-                    rep += (f"\tassert( a({i}, {j}, {k}, {l}).data()[0] == {t[i, j, k, l]});  ")
-                    rep += "\tstd::cout << '.';\n"
+                    rep += (f"\tCHECK( a({i}, {j}, {k}, {l}).data()[0] == {t[i, j, k, l]});  ")
     rep += "};"
-    rep += """
-int main() {
-    test_indexing_from_pytorch();
-    return 0;
-}
-"""
     f.write(rep);
 
 
