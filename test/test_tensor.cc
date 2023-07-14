bb4e3661104a7ae6bcdfd24a358de42e719ceb76
Octavian Rusu <octavian.concept@gmail.com>
2023-07-14 15:45:16 -0600

    Add API tests for getters, reshape() and expand(). Modify existing tests to recent API changes

--- test/test_tensor.cc
+++ test/test_tensor.cc
@@ -20,7 +20,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -37,7 +37,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -54,7 +54,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -71,7 +71,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -88,7 +88,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -105,7 +105,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -122,7 +122,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -139,7 +139,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -156,7 +156,7 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				CHECK(a.data());
 				CHECK(a.ndim() == 3);
 				std::initializer_list<uint32_t> c = {2, 2, 512};
-				std::shared_ptr<uint32_t[]> shp = a.get_shape();
+				std::shared_ptr<uint32_t[]> shp = a.view();
 				uint32_t i=0;
 				for(const auto &x : c) {
 					CHECK(shp[i] == x);
@@ -164,7 +164,6 @@ TEST_CASE("Tensor Constructor", "[core]") {
 				}
 			}
 		}
-
 		SECTION("Incorrect shape") {
 			std::unique_ptr<float[]> data = std::make_unique<float[]>(N);
 			for(size_t i=0; i < N; i++) { data[i]=i; }
@@ -172,10 +171,10 @@ TEST_CASE("Tensor Constructor", "[core]") {
 		}
 
 		// PASS, not implemented
-		SECTION("Grad") {}
 		SECTION("Test Storage GPU") {}
 		SECTION("Test Storage CPU") {}
 	}
+
 	SECTION("Test initializer_list constructor") {
 		SECTION("Correct") {}
 		SECTION("Incorrect shape") {}
@@ -183,16 +182,155 @@ TEST_CASE("Tensor Constructor", "[core]") {
 		SECTION("Test Storage GPU") {}
 		SECTION("Test Storage CPU") {}
 	}
+
 	SECTION("Test sized_array shape constructor") {
 		SECTION("Correct") {}
 	}
 }
 
+TEST_CASE("API", "[core]") {
+	std::unique_ptr<float[]> data = std::make_unique<float[]>(N*N);
+	std::initializer_list<uint32_t> sp = {N, N};
+	std::initializer_list<uint32_t> st = {N, 1};
+	for(size_t i=0; i < N*N; i++) { data[i]=i; }
+	Tensor<float> a(data, N*N, {N, N});
+	uint32_t i = 0;
+
+	SECTION("Getters") {
+		SECTION("View") {
+			CHECK_NOTHROW(a.view());
+			auto view = a.view();
+			i=0;
+			SECTION("Correctness") { for(const auto& x : sp) { CHECK(view[i] == x); } }
+
+			SECTION("Invalid Assignment") {
+				// This does not throw
+				// TODO: Find way to stop this
+				// CHECK_THROWS(view[0] = 0);
+			}
+			SECTION("Iteration") {
+				// No clue how yet	
+			}
+		}
+	}
+}
+
 TEST_CASE("Tensor OPs", "[core]") {
-	SECTION("Stride") {}
-	SECTION("Reshape") {}
-	SECTION("Expand") {}
-	SECTION("Permute") {}
+
+	// SECTION("Stride") { }
+	
+	SECTION("Reshape") {
+		std::unique_ptr<float[]> data = std::make_unique<float[]>(N*N);
+		std::initializer_list<uint32_t> sp = {N, N};
+		std::initializer_list<uint32_t> st = {N, 1};
+		for(size_t i=0; i < N*N; i++) { data[i]=i; }
+		Tensor<float> a(data, N*N, {N, N});
+		uint32_t i = 0;
+
+		SECTION("Correct") {
+			CHECK_NOTHROW(a.reshape({N/2, 2, N/2, 2})); 
+			std::initializer_list<uint32_t> tsp = {N/2, 2, N/2, 2};
+			std::initializer_list<uint32_t> tst = { (N/2)*2*2,(N/2)*2, 2, 1 };
+			i=0;
+			for(const auto& x : tsp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : tst) { CHECK(a.strides()[i] == x); i++; }
+		}
+
+		CHECK_NOTHROW(a.reshape({N, N}));
+		i=0;
+		for(const auto& x : sp) { CHECK(a.view()[i] == x); i++; }
+		i=0;
+		for(auto const& x : st) { CHECK(a.strides()[i] == x); i++; }
+
+		SECTION("Invalid Product") {
+			CHECK_THROWS(a.reshape({N, 1}));	
+			i=0;
+			for(const auto& x : sp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : st) { CHECK(a.strides()[i] == x); i++; }
+		}
+
+		SECTION("Argument 0") {
+			CHECK_THROWS(a.reshape({N, N, 0}));	
+			i=0;
+			for(const auto& x : sp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : st) { CHECK(a.strides()[i] == x); i++; }
+		}
+
+		SECTION("Stringify") {
+			CHECK_NOTHROW(a.reshape({N*N, 1}));
+			std::initializer_list<uint32_t> tsp = {N*N, 1};
+			std::initializer_list<uint32_t> tst = {1, 1};
+			i=0;
+			for(const auto& x : tsp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : tst) { CHECK(a.strides()[i] == x); i++; }
+
+			CHECK_NOTHROW(a.reshape({1, N*N}));
+			tsp = {1, N*N};
+			tst = {N*N, 1};
+			i=0;
+			for(const auto& x : tsp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : tst) { CHECK(a.strides()[i] == x); i++; }
+		}
+	}
+
+	SECTION("Expand") {
+		std::unique_ptr<float[]> data = std::make_unique<float[]>(N);
+		std::initializer_list<uint32_t> sp = {N, 1};
+		std::initializer_list<uint32_t> st = {1, 1};
+		for(size_t i=0; i < N; i++) { data[i]=i; }
+		Tensor<float> a(data, N, sp);
+		uint32_t i = 0;
+
+		SECTION("Correct") {
+			CHECK_NOTHROW(a.expand({N, 5}));
+			std::initializer_list<uint32_t> tsp = {N, 5};
+			std::initializer_list<uint32_t> tst = {1, 0};
+			i=0;
+			for(const auto& x : tsp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : tst) { CHECK(a.strides()[i] == x); i++; }
+
+			CHECK_NOTHROW(a(0, 0));
+			CHECK_NOTHROW(a(0, 1));
+			CHECK_NOTHROW(a(0, 2));
+			CHECK_NOTHROW(a(0, 3));
+			CHECK_NOTHROW(a(0, 4));
+			CHECK(a(0,0).data()[0]==a(0,1).data()[0]);
+			CHECK(a(0,1).data()[0]==a(0,2).data()[0]);
+			CHECK(a(0,2).data()[0]==a(0,3).data()[0]);
+			CHECK(a(0,3).data()[0]==a(0,4).data()[0]);
+		}
+
+		SECTION("Expansion on non-1 dimension") {
+			a.reshape({N/2, 2});
+			std::initializer_list<uint32_t> tsp = {N/2, 2};
+			std::initializer_list<uint32_t> tst = {2, 1};
+			CHECK_THROWS(a.expand({N/2, 5}));
+			i=0;
+			for(const auto& x : tsp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : tst) { CHECK(a.strides()[i] == x); i++; }
+		}
+
+		a.reshape({N, 1});
+
+		SECTION("Wrong argument for other dimensions") {
+			CHECK_THROWS(a.expand({N/2, 5}));
+			i=0;
+			for(const auto& x : sp) { CHECK(a.view()[i] == x); i++; }
+			i=0;
+			for(auto const& x : st) { CHECK(a.strides()[i] == x); i++; }
+		}
+	}
+
+	SECTION("Permute") {
+
+	}
 }
 
 TEST_CASE("Single Tensor OPs", "[core]") {} 
