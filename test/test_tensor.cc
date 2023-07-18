4b46bc9ecc56b52aef4669fd0297c0abae4f642a
Octavian Rusu <octavian.concept@gmail.com>
2023-07-18 16:22:33 -0600

    test arange and fill

--- test/test_tensor.cc
+++ test/test_tensor.cc
@@ -4,8 +4,10 @@
 #include "tensor.h"
 
 #define N 2048
+#define EPSILON 0.001
 
 using namespace tensor;
+using Catch::Matchers::Floating::WithinAbsMatcher;
 
 TEST_CASE("Tensor Constructor", "[core]") {
 
@@ -239,6 +241,102 @@ TEST_CASE("Tensor Constructor", "[core]") {
 	SECTION("Test sized_array shape constructor") {
 		SECTION("Correct") {}
 	}
+
+	SECTION("Construction Helpers") {
+		SECTION("fill") {
+			Tensor<float> a = Tensor<float>({N, N});
+			CHECK_NOTHROW(a.fill(1.25));
+			CHECK(a.is_initialized);
+			CHECK(a.device == CPU);
+			CHECK(a.size == N*N);
+			CHECK(a.data());
+			CHECK(a.strides());
+			CHECK(a.view());
+			CHECK(a.ndim() == 2);
+			std::initializer_list<uint32_t> c = {N, N};
+			std::shared_ptr<uint32_t[]> shp = a.view();
+			uint32_t k=0;
+			for(const auto &x : c) {
+				CHECK(shp[k] == x);
+				k++;
+			}
+			for(size_t i=0; i < a.view()[0]; i++) {
+				for(size_t j=0; j < a.view()[1]; j++) {
+					CHECK_THAT(a(i, j).data()[0], WithinAbsMatcher(1.25, EPSILON));
+				}
+			}
+		}
+
+		SECTION("arange") {
+			CHECK_NOTHROW(Tensor<float>::arange(50));
+			CHECK_NOTHROW(Tensor<float>::arange(50, 10));
+			CHECK_NOTHROW(Tensor<float>::arange(50, 10, 5));
+
+			CHECK_THROWS(Tensor<float>::arange(10, 20));
+			CHECK_THROWS(Tensor<float>::arange(10, 0, 15));
+			CHECK_THROWS(Tensor<float>::arange(10, 0, 10));
+
+			SECTION("Top limit only") {
+				Tensor<int8_t> a = Tensor<int8_t>::arange(50); 
+				CHECK(a.is_initialized);
+				CHECK(a.device == CPU);
+				CHECK(a.size == 50);
+				CHECK(a.data());
+				CHECK(a.strides());
+				CHECK(a.view());
+				CHECK(a.ndim() == 1);
+				std::initializer_list<uint32_t> c = {50};
+				std::shared_ptr<uint32_t[]> shp = a.view();
+				uint32_t j=0;
+				for(const auto &x : c) {
+					CHECK(shp[j] == x);
+					j++;
+				}
+				auto data = a.data();
+				for(int8_t i=0; i < a.size; i++) {
+					CHECK(data[i]==i);	
+				}
+			}
+
+			SECTION("Top and negative bottom limit") {
+				Tensor<int> a = Tensor<int>::arange(30, -20);
+				CHECK(a.size == 50);
+				auto data = a.data();
+				for(int i=0; i < a.size; i++) {
+					CHECK(data[i]==i-20);	
+				}
+			}
+
+			SECTION("Top, botom and int step") {
+				Tensor<int> a = Tensor<int>::arange(30, -20, 5);
+				CHECK(a.size == 50/5);
+				auto data = a.data();
+				uint32_t inc = 0.f;
+				for(int i=0; i < a.size; i++) {
+					CHECK(data[i]==i-20+inc);	
+					inc+=4;
+				}
+			}
+
+			SECTION("Top and botom int, float step") {
+				Tensor<float> a = Tensor<float>::arange(20, -30, 0.1f); 
+				CHECK(a.size == 50/0.1f);
+				auto data = a.data();
+				float inc = -30.f;
+				for(int i=0; i < a.size; i++) {
+					CHECK_THAT(data[i], WithinAbsMatcher(inc, EPSILON));
+					inc+=0.1f;
+				}
+			}
+		}
+
+		SECTION("randn") {
+			SECTION("static") {
+
+			}
+		}
+		SECTION("eye") {}
+	}
 }
 
 TEST_CASE("API", "[core]") {
@@ -267,16 +365,6 @@ TEST_CASE("API", "[core]") {
 		}
 	}
 
-	SECTION("Construction Helpers") {
-		SECTION("fill") {
-			Tensor<float> a = Tensor<float>({N, N});
-			CHECK_NOTHROW(a.fill(1.25));
-		}
-
-		SECTION("arange") {}
-		SECTION("randn") {}
-		SECTION("eye") {}
-	}
 }
 
 TEST_CASE("Tensor OPs", "[core]") {
