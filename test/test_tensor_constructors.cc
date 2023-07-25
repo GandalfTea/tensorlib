fce0d1862d9f0c8967592341496bae42dc8c548d
Octavian Rusu <octavian.concept@gmail.com>
2023-07-24 23:56:14 -0600

    add tests for new constructors

--- test/test_tensor_constructors.cc
+++ test/test_tensor_constructors.cc
@@ -14,20 +14,45 @@ using Catch::Matchers::Floating::WithinAbsMatcher;
 TEST_CASE("Tensor Constructor", "[core]") {
 
 	SECTION("Test virtual tensor constructor") {
-		Tensor<float> a({N, N});
-		CHECK(!a.data());
-		CHECK(!a.is_initialized);
-		CHECK(a.strides());
-		CHECK(a.view());
-		CHECK(a.size() == N*N);
-		CHECK(a.ndim()==2);
-		CHECK(a.device == CPU);
-		std::initializer_list<uint32_t> shp = {N, N};
-		std::shared_ptr<uint32_t[]> cshp = a.view();
-		uint32_t i=0;
-		for(const auto &x : shp) {
-			CHECK(cshp[i] == x);
-			i++;
+		SECTION("initializer_list") {
+			Tensor<float> a({N, N});
+			CHECK(!a.data());
+			CHECK(!a.is_initialized);
+			CHECK(a.strides());
+			CHECK(a.view());
+			CHECK(a.size() == N*N);
+			CHECK(a.ndim()==2);
+			CHECK(a.device == CPU);
+			std::initializer_list<uint32_t> shp = {N, N};
+			std::shared_ptr<uint32_t[]> cshp = a.view();
+			uint32_t i=0;
+			for(const auto &x : shp) {
+				CHECK(cshp[i] == x);
+				i++;
+			}
+		}
+
+		SECTION("sized_array") {
+			sized_array<uint32_t> s;
+			s.ptr = std::unique_ptr<uint32_t[]>(new uint32_t[2]());
+			s.ptr[0] = N;
+			s.ptr[1] = N;
+			s.size = 2;
+			Tensor<float> a(s);
+			CHECK(!a.data());
+			CHECK(!a.is_initialized);
+			CHECK(a.strides());
+			CHECK(a.view());
+			CHECK(a.size() == N*N);
+			CHECK(a.ndim()==2);
+			CHECK(a.device == CPU);
+			std::initializer_list<uint32_t> shp = {N, N};
+			std::shared_ptr<uint32_t[]> cshp = a.view();
+			uint32_t i=0;
+			for(const auto &x : shp) {
+				CHECK(cshp[i] == x);
+				i++;
+			}
 		}
 	}
 
@@ -36,9 +61,10 @@ TEST_CASE("Tensor Constructor", "[core]") {
 			SECTION("No template argument") {
 				std::unique_ptr<float[]> data = std::unique_ptr<float[]>( new float[N]());
 				CHECK_NOTHROW(Tensor<>(data, N, {2, 2, N/4}));
-				CHECK_NOTHROW(Tensor<>::arange(50));
-				CHECK_NOTHROW(Tensor<>::randn({50, 50}));
+				auto a = Tensor<>(data, N, {2, 2, N/4});
+				CHECK(typeid(a.data()[0]) == typeid(0.f));
 			}
+
 			SECTION("uint8_t") {
 				std::unique_ptr<uint8_t[]> data = std::make_unique<uint8_t[]>(N);
 				for(size_t i=0; i < N; i++) { data[i]=i; }
@@ -191,10 +217,29 @@ TEST_CASE("Tensor Constructor", "[core]") {
 					i++;
 				}
 			}
-			SECTION("float") {
+			SECTION("float32") {
 				std::unique_ptr<float[]> data = std::make_unique<float[]>(N);
 				for(size_t i=0; i < N; i++) { data[i]=i; }
 				Tensor<float> a(data, N, {2, 2, N/4});
+				CHECK(a.is_initialized);
+				CHECK(a.device == CPU);
+				CHECK(a.size() == N);
+				CHECK(a.data());
+				CHECK(a.strides());
+				CHECK(a.view());
+				CHECK(a.ndim() == 3);
+				std::initializer_list<uint32_t> c = {2, 2, N/4};
+				std::shared_ptr<uint32_t[]> shp = a.view();
+				uint32_t i=0;
+				for(const auto &x : c) {
+					CHECK(shp[i] == x);
+					i++;
+				}
+			}
+			SECTION("float64") {
+				std::unique_ptr<double[]> data = std::make_unique<double[]>(N);
+				for(size_t i=0; i < N; i++) { data[i]=i; }
+				Tensor<double> a(data, N, {2, 2, N/4});
 				CHECK(a.is_initialized);
 				CHECK(a.device == CPU);
 				CHECK(a.size() == N);
@@ -216,12 +261,10 @@ TEST_CASE("Tensor Constructor", "[core]") {
 			for(size_t i=0; i < N; i++) { data[i]=i; }
 			CHECK_THROWS(Tensor<float>(data, N, {1, 2, N/4}));
 		}
-
-		SECTION("Test Storage CPU") {}
 	}
 
 	SECTION("Test initializer_list constructor") {
-		SECTION("Correct") {
+		SECTION("float") {
 			Tensor<float> a({0, 1, 2, 3, 4, 5}, {2, 3});
 			CHECK(a.is_initialized);
 			CHECK(a.device == CPU);
@@ -242,12 +285,57 @@ TEST_CASE("Tensor Constructor", "[core]") {
 		SECTION("Incorrect shape") {
 			CHECK_THROWS(Tensor<float>({0.f, 1.f, 2.f, 3.f, 4.f, 5.f}, {2, 2, 2}));
 		}
-
-		SECTION("Test Storage CPU") {}
 	}
 
 	SECTION("Test sized_array shape constructor") {
-		SECTION("Correct") {}
+		SECTION("initializer_list") {
+			sized_array<uint32_t> s;
+			s.ptr = std::unique_ptr<uint32_t[]>(new uint32_t[2]());
+			s.ptr[0] = 2;
+			s.ptr[1] = 3;
+			s.size = 2;
+			Tensor<float> a({0, 1, 2, 3, 4, 5}, s);
+			CHECK(a.is_initialized);
+			CHECK(a.device == CPU);
+			CHECK(a.size() == 6);
+			CHECK(a.data());
+			CHECK(a.strides());
+			CHECK(a.view());
+			CHECK(a.ndim() == 2);
+			std::initializer_list<uint32_t> c = {2, 3};
+			std::shared_ptr<uint32_t[]> shp = a.view();
+			uint32_t i=0;
+			for(const auto &x : c) {
+				CHECK(shp[i] == x);
+				i++;
+			}
+		}
+
+		SECTION("unique_ptr") {
+			sized_array<uint32_t> s;
+			s.ptr = std::unique_ptr<uint32_t[]>(new uint32_t[3]());
+			s.ptr[0] = 2;
+			s.ptr[1] = 2;
+			s.ptr[2] = N/4;
+			s.size = 3;
+			std::unique_ptr<float[]> data = std::make_unique<float[]>(N);
+			for(size_t i=0; i < N; i++) { data[i]=i; }
+			Tensor<float> a(data, N, s);
+			CHECK(a.is_initialized);
+			CHECK(a.device == CPU);
+			CHECK(a.size() == N);
+			CHECK(a.data());
+			CHECK(a.strides());
+			CHECK(a.view());
+			CHECK(a.ndim() == 3);
+			std::initializer_list<uint32_t> c = {2, 2, N/4};
+			std::shared_ptr<uint32_t[]> shp = a.view();
+			uint32_t i=0;
+			for(const auto &x : c) {
+				CHECK(shp[i] == x);
+				i++;
+			}
+		}
 	}
 }
 
