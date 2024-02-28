93c4c4760b673b07cc3dfe1e837261c5d8b9d95e
GandalfTea <octavian.concept@gmail.com>
2024-02-28 01:44:22 -0700

    more tests + they all pass

--- test/test_ntensor_constructors.cc
+++ test/test_ntensor_constructors.cc
@@ -128,16 +128,16 @@ TEST_CASE("Virtual Tensors") {
 #endif
 #ifdef __SSE__ 
       SECTION("SSE") {
-        #undef DATA_ALIGNMENT
-        #define DATA_ALIGNMENT 16
+        #undef MEMORY_ALIGNMENT
+        #define MEMORY_ALIGNMENT 16
         Tensor<float> a = Tensor<float>({N, N}).allocate();
         CHECK_NOTHROW(_mm_load_ps(&(a.storage())[0]));
       }
 #endif 
 #ifdef __AVX512F__
       SECTION("AVX-512") {
-        #undef DATA_ALIGNMENT
-        #define DATA_ALIGNMENT 64; 
+        #undef MEMORY_ALIGNMENT
+        #define MEMORY_ALIGNMENT 64; 
         Tensor<float> a = Tensor<float>({N, N}).allocate();
         CHECK_NOTHROW(_mm512_load_ps(&(a.storage())[0]));
       }
@@ -196,10 +196,30 @@ TEST_CASE("Virtual Tensors") {
     }
   }
 }
+#undef MEMORY_ALIGNMENT
+#define MEMORY_ALIGNMENT 32
 
 TEST_CASE("Data initialization") {
+  SECTION("<alignment empty malloc") {
+    float* d = alloc<float>(2);
+    Tensor<float> a = Tensor<float>(d, 2, {2});
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == 2);
+    CHECK(a.numel() == 2);
+    CHECK(a.ndim() == 1);
+    CHECK(a.storage() != nullptr);
+    CHECK(a.shape()[0] == 2);
+    CHECK(a.strides()[0] == 1);
+    CHECK_NOTHROW(a(0));
+    CHECK_NOTHROW(a(1));
+  }
   SECTION("empty malloc") {
-    float* d = static_cast<float*>( aligned_alloc(32, N*N*sizeof(float)));
+    float* d = alloc<float>(N*N);
     Tensor<float> a = Tensor<float>(d, N*N, {N, N});
     CHECK(a.is_initialized());
     CHECK(a.is_allocated());
@@ -219,5 +239,78 @@ TEST_CASE("Data initialization") {
     CHECK_NOTHROW(a(N-1));
     CHECK_NOTHROW(a(N-1, N-1));
   }
+  SECTION("aligned_alloc malloc") {
+    float* d = static_cast<float*>(aligned_alloc(MEMORY_ALIGNMENT, N*N*sizeof(float)));
+    Tensor<float> a = Tensor<float>(d, N*N, {N, N});
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    CHECK(a.storage() != nullptr);
+    CHECK(a.shape()[0] == N);
+    CHECK(a.shape()[1] == N);
+    CHECK(a.strides()[0] == N);
+    CHECK(a.strides()[1] == 1);
+    CHECK_NOTHROW(a(0));
+    CHECK_NOTHROW(a(N-1));
+    CHECK_NOTHROW(a(N-1, N-1));
+  }
+
+  #undef N
+  #define N 481
+  SECTION("aligned_alloc random num malloc") {
+    float* d = static_cast<float*>(aligned_alloc(MEMORY_ALIGNMENT, N*N*sizeof(float)));
+    Tensor<float> a = Tensor<float>(d, N*N, {N, N});
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    CHECK(a.storage() != nullptr);
+    CHECK(a.shape()[0] == N);
+    CHECK(a.shape()[1] == N);
+    CHECK(a.strides()[0] == N);
+    CHECK(a.strides()[1] == 1);
+    CHECK_NOTHROW(a(0));
+    CHECK_NOTHROW(a(N-1));
+    CHECK_NOTHROW(a(N-1, N-1));
+  }
+  SECTION("shape mismatch small") {
+    float* d = alloc<float>(N*N);
+    CHECK_THROWS(Tensor<float>(d, N*N, {1, N}));
+    free(d);
+  }
+  SECTION("shape mismatch big") {
+    float* d = alloc<float>(N*N);
+    CHECK_THROWS(Tensor<float>(d, N*N, {N, N, N}));
+    free(d);
+  }
+  SECTION("wrong size small") {
+    float* d = alloc<float>(N*N);
+    CHECK_THROWS(Tensor<float>(d, N, {N, N}));
+    free(d);
+  }
+  SECTION("wrong size big") {
+    float* d = alloc<float>(N*N);
+    CHECK_THROWS(Tensor<float>(d, N*N*2, {N, N}));
+    free(d);
+  }
+  // TODO: this is technically valid
+  /*
+  SECTION("both wrong size and shape") {
+    float* d = alloc<float>(N*N);
+    CHECK_THROWS(Tensor<float>(d, N*N*2, {N, N, 2}));
+    free(d);
+  }
+  */
 }
 
