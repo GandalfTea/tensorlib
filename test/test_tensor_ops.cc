5171d9d07f35f67042555b4ad555d036ce4a08ef
GandalfTea <octavian.concept@gmail.com>
2024-03-10 16:15:37 -0600

    unary ops tests

--- test/test_tensor_ops.cc
+++ test/test_tensor_ops.cc
@@ -8,13 +8,97 @@ using namespace tensorlib;
 #define N 1024
 #define EPSILON 0.0001
 
-
 TEST_CASE("Unary OPs") {
-  SECTION("sqrt") {}
-  SECTION("exp2") {}
-  SECTION("log2") {}
-  SECTION("sin") {}
-  SECTION("neg") {}
+  SECTION("sqrt") {
+    float* org = alloc<float>(N*N);
+    auto a = Tensor<float>({N, N}).randn(100, 1, UNIFORM);
+    std::memcpy(org, a.storage(), N*N*sizeof(float));
+    a.sqrt();
+    auto aptr = a.storage();
+    for(size_t i=0; i<N*N; i++) CHECK(eql_f32(aptr[i], std::sqrt(org[i])));
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    free(org);
+  }
+  SECTION("exp2") {
+    float* org = alloc<float>(N*N);
+    auto a = Tensor<float>({N, N}).randn();
+    std::memcpy(org, a.storage(), N*N*sizeof(float));
+    a.exp2();
+    auto aptr = a.storage();
+    for(size_t i=0; i<N*N; i++) CHECK(eql_f32(aptr[i], std::exp2(org[i])));
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    free(org);
+  }
+  SECTION("log2") {
+    float* org = alloc<float>(N*N);
+    auto a = Tensor<float>({N, N}).randn(100, 1, UNIFORM);
+    std::memcpy(org, a.storage(), N*N*sizeof(float));
+    a.log2();
+    auto aptr = a.storage();
+    for(size_t i=0; i<N*N; i++) CHECK(eql_f32(aptr[i], std::log2(org[i])));
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    free(org);
+  }
+  SECTION("sin") {
+    float* org = alloc<float>(N*N);
+    auto a = Tensor<float>({N, N}).randn();
+    std::memcpy(org, a.storage(), N*N*sizeof(float));
+    a.sin();
+    auto aptr = a.storage();
+    for(size_t i=0; i<N*N; i++) CHECK(eql_f32(aptr[i], std::sin(org[i])));
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    free(org);
+  }
+  SECTION("neg") {
+    float* org = alloc<float>(N*N);
+    auto a = Tensor<float>({N, N}).randn();
+    std::memcpy(org, a.storage(), N*N*sizeof(float));
+    a.neg();
+    auto aptr = a.storage();
+    for(size_t i=0; i<N*N; i++) CHECK(eql_f32(aptr[i], (-1*org[i])));
+    CHECK(a.is_initialized());
+    CHECK(a.is_allocated());
+    CHECK(!a.has_grad());
+    CHECK(!a.is_eye());
+    CHECK(!a.is_sub());
+    CHECK(a.device() == 1);
+    CHECK(a.memsize() == N*N);
+    CHECK(a.numel() == N*N);
+    CHECK(a.ndim() == 2);
+    free(org);
+  }
 }
 
 TEST_CASE("Binary OPs") {
